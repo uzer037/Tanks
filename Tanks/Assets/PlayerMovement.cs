adb6e4ff8d5e771e1c8c7b768bc6473d960a633b
sergeyrid0 <sergeyrid@yandex.ru>
2019-11-20 18:00:26 +0300

    init

---
+++ Tanks/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CharacterController controller;
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(0,0);
+        Vector2 gvel = new Vector2(0,0);
+        if(Input.GetKey(KeyCode.W))
+        {
+            movement += new Vector2(0,20f) * Time.deltaTime;
+        }
+        gvel -= new Vector2(0,10f) * Time.deltaTime;
+        movement += gvel;
+        controller.Move(movement);
+        transform.position = new Vector3(transform.position.x,transform.position.y,0);
+    }
+}
---
+++ Tanks/Assets/terrain.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Mesh mesh;
+    public Material material;
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+    MeshCollider meshCollider;
+    void Start()
+    {
+        meshFilter = gameObject.GetComponent<MeshFilter>();
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        meshCollider = gameObject.GetComponent<MeshCollider>();
+        mesh = new Mesh();
+        
+        Vector3[] points = {new Vector2(0,0), new Vector2(0,1), new Vector2(1,0), new Vector2(1,1), new Vector2(2,0.25f)};
+        int[] tri = {
+            0,1,2,
+            1,2,3,
+            2,4,3
+        };
+
+        mesh.vertices = points;
+        mesh.triangles = tri;
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+        mesh.RecalculateBounds();
+        meshFilter.mesh = mesh;
+        meshRenderer.material = material;
+        meshCollider.sharedMesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
